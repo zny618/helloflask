4b12fcf99c4873c1ac1c3ec7c8a741fdd6aff49e
zny <320223370@xjau.edu.cn>
2024-12-27 01:53:08 +0800

    Add index page

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is a hellofalsk learning record
+[hellofalsk](https://tutorial.helloflask.com) is a quick learning falskdemo useing chinese.
--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for
 from markupsafe import escape
 
 
+
 app = Flask(__name__)
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 @app.route('/')
 @app.route('/index')
-def hello():
-    return '<h1>hello flask!<h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+     return render_template('index.html', name=name, movies=movies)
+    # return '<h1>hello flask!<h1><img src="http://helloflask.com/totoro.gif">'
 @app.route('/home')
 def home():
     return 'This is a home show!'
@@ -19,7 +36,7 @@ def user_page(name):
 @app.route('/test')
 def test_url_for():
     # follow is some example(http://localhost:5000/test after cmd windows print URL)
-    print(url_for('hello'))                 # print:/index
+    print(url_for('index'))                 # print:/index
     print(url_for('user_page', name='zny'))   # print:/user/zny
     print(url_for('user_page', name='xiaoyu')) #print:/user/xiaoyu
     print(url_for('test_url_for')) # print:/test
