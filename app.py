599e8718a7b136ba1799e79337d6bdfbeb2baa95
zny <320223370@xjau.edu.cn>
2025-01-06 11:11:18 +0800

    Add database support with Flask-SQAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 from flask import url_for
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+import click
+import os
+import sys
 
-
+prefix = 'sqlite:///'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
 
 name = 'Grey Li'
 movies = [
@@ -20,11 +37,45 @@ movies = [
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
 
+import click
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-     return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', name=name, movies=movies)
     # return '<h1>hello flask!<h1><img src="http://helloflask.com/totoro.gif">'
 @app.route('/home')
 def home():
